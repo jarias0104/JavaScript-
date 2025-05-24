002dbe3cf0c42919da59d65302448d2e40d599fb
Jeshua Arias <ariasjeshua2@gmail.com>
2025-05-24 12:44:54 -0600

    Add files via upload

---
+++ logicalOperation.js
@@ -0,0 +1,4 @@
+//declare a variable with a number and use addition to make a bigger number
+const myNumber = 1;
+let additionOperation = myNumber + 1;
+console.log("Here is the result of the addition:", additionOperation);
